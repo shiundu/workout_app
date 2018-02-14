e86a883dda8c5a7728e8dc615bfef450e8ab9499
Ben Shiundu <benshiundu@gmail.com>
2018-02-14 18:53:11 +0300

    redirected to start with main

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Main from './app/containers/main';
 
-AppRegistry.registerComponent('workout_app', () => App);
+AppRegistry.registerComponent('workout_app', () => Main);
