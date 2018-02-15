073a26dbb4be86456a2822a0a1cc2a57c60824b4
Ben Shiundu <benshiundu@gmail.com>
2018-02-15 13:37:53 +0300

    list workouts from the store

--- app/containers/all-workouts.js
+++ app/containers/all-workouts.js
@@ -3,12 +3,13 @@ import { bindActionCreators } from 'redux';
 import { connect  } from 'react-redux';
 import { ActionCreators } from '../actions';
 import { Actions } from 'react-native-router-flux';
-import { Icon } from 'react-native-elements';
-import Colors from '../styles/colors';
+import AllWorkOutStyles from '../styles/allworkout-styles';
 import {
     View,
     Text,
     TextInput,
+    FlatList,
+    TouchableOpacity
 } from 'react-native';
 
 function mapStateToProps(state) {
@@ -24,14 +25,30 @@ class AllWorkOuts extends Component {
   constructor(){
     super();
   }
+  _onPress =  (item)=>{
+    this.props.setActive(item);
+    Actions.WorkOut();
+  }
+  _keyExtractor = (item, index) => item.id;
+  _renderItem = ({item}) => (
 
+    <TouchableOpacity onPress={() => this._onPress(item)} style={AllWorkOutStyles.workutContent}>
+      <View style={AllWorkOutStyles.workOutView}>
+        <Text style={AllWorkOutStyles.workoutText}>{item.name}</Text>
+      </View>
+    </TouchableOpacity>
+  );
   render(){
-    let { currentUser } = this.props;
+    let { allworkouts } = this.props;
     return (
-          <View>
-            <Text>All Work Out</Text>
-          </View>
-      );
+      <View style={AllWorkOutStyles.container}>
+        <FlatList
+          data={allworkouts.workouts}
+          keyExtractor={this._keyExtractor}
+          renderItem={this._renderItem}
+        />
+      </View>
+    );
   }
 }
 
